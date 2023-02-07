aaad7a9aaf0fb64b3bbe3ee98567af9ccd237868
Mathieu Furlan <mathieu.furlan@inseec-france.com>
2023-02-07 17:24:01 +0100

    Second version with working redirection in case of group matching search string

--- optionsDeal.js
+++ optionsDeal.js
@@ -1,4 +1,43 @@
-let elems = document.getElementsByTagName("input");
-let formu = document.forms[0];
-elems[0].value = "pc portables";
-formu.submit();
+function premPage(formu){
+    let valid = formu.item(0);
+    let elems = document.getElementsByName("q");
+    elems.item(0).value = "pc portables";
+    valid.submit();
+}
+function apresRech(groupes){
+    console.log(groupes);
+    console.log(groupes.length);
+    for(let obj of groupes){
+        if(obj.title.toLowerCase() == "pc portables".toLowerCase()){
+            window.location.replace(obj.href);
+        }
+    }
+}
+if(window.location.href === "https://www.dealabs.com/"){
+    const observer = new MutationObserver(function (mutations, mutationInstance){
+        const formu = document.getElementsByTagName("form");
+        if(formu){
+            premPage(formu);
+            mutationInstance.disconnect();
+        }
+    });
+
+    observer.observe(document, {
+        childList: true,
+        subtree: true
+    });
+}
+else if(window.location.href.indexOf("search?q=") != -1){
+    const observer = new MutationObserver(function (mutations, mutationInstance){
+        const groupes = document.getElementsByClassName("button button--type-tag button--shape-rounded button--mode-light");
+        if(groupes.length > 0){
+            apresRech(groupes);
+            mutationInstance.disconnect();
+        }
+    });
+
+    observer.observe(document, {
+        childList: true,
+        subtree: true
+    });
+}
